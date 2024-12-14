79abc06513d8eaf41dcec4e4884d02de35a05ae8
Akira R Kinjo <993982+arkinjo@users.noreply.github.com>
2024-12-14 17:29:25 +0900

    more info

--- multicell/analys.go
+++ multicell/analys.go
@@ -2,7 +2,11 @@ package multicell
 
 import (
 	"fmt"
+	"log"
+	"math"
 	"os"
+
+	"gonum.org/v1/gonum/mat"
 )
 
 func (s *Setting) GetPhenoAxis(env0, env1 Environment) Vec {
@@ -14,6 +18,23 @@ func (s *Setting) GetPhenoAxis(env0, env1 Environment) Vec {
 	VecScale(dv, 1/mag2)
 
 	return dv
+}
+
+func avesd(vec Vec) (float64, float64) {
+	a := 0.0
+	n := float64(len(vec))
+	for _, x := range vec {
+		a += x
+	}
+
+	a /= n
+	v := 0.0
+	for _, x := range vec {
+		v += (x - a) * (x - a)
+	}
+	v = math.Sqrt(v / n)
+
+	return a, v
 }
 
 func AverageVecs(vecs []Vec) Vec {
@@ -44,13 +65,50 @@ func (pop *Population) ProjectGenoPheno(s *Setting, filename string,
 	JustFail(err)
 	defer fout.Close()
 
-	for i, indiv := range pop.Indivs {
+	var gs, ps, ns, ms Vec
+
+	for _, indiv := range pop.Indivs {
 		gt := indiv.Genome.ToVec(s)
 		pt := indiv.SelectedPhenotypeVec(s)
 		DiffVecs(gt, g0, gt)
 		DiffVecs(pt, p0, pt)
-		gc := DotVecs(gt, gaxis)
-		pc := DotVecs(pt, paxis)
-		fmt.Fprintf(fout, "%d\t%f\t%f\n", i, gc, pc)
+		gs = append(gs, DotVecs(gt, gaxis))
+		ps = append(ps, DotVecs(pt, paxis))
+		ns = append(ns, float64(indiv.Ndev))
+		ms = append(ms, indiv.Mismatch)
 	}
+	ga, gv := avesd(gs)
+	pa, pv := avesd(ps)
+	na, nv := avesd(ns)
+	ma, mv := avesd(ms)
+	fmt.Fprintf(fout, "P\t%d\t%d\t%f\t%f\t%f\t%f\t%f\t%f\t%f\t%f\n",
+		pop.Igen, len(gs), ga, pa, gv, pv, na, nv, ma, mv)
+	for i, indiv := range pop.Indivs {
+		fmt.Fprintf(fout, "I\t%d\t%f\t%f\t%d\t%f\n",
+			i, gs[i], ps[i], indiv.Ndev, ms[i])
+	}
+}
+
+func CovarianceMatrix(xs []Vec, x0 Vec, ys []Vec, y0 Vec) *mat.Dense {
+	if len(xs) != len(ys) {
+		log.Printf("CovarianceMatrix: size mismatch %d != %d\n",
+			len(xs), len(ys))
+		panic("CovarianceMatrix")
+	}
+	nx := len(x0)
+	ny := len(y0)
+	xt := make(Vec, nx)
+	yt := make(Vec, ny)
+	cov := mat.NewDense(nx, ny, nil)
+	for n, x := range xs {
+		DiffVecs(x, x0, xt)
+		DiffVecs(ys[n], y0, yt)
+		for i := range nx {
+			for j := range ny {
+				cov.Set(i, j, xt[i]*yt[j])
+			}
+		}
+	}
+
+	return cov
 }
