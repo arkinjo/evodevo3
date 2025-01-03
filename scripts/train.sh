3e2f4dcea6bca187eb3a68f610a88f7afcdc70b1
Akira R Kinjo <993982+arkinjo@users.noreply.github.com>
2025-01-03 17:11:19 +0900

    Update train.sh

--- scripts/train.sh
+++ scripts/train.sh
@@ -1,6 +1,6 @@
 #!/bin/zsh
 
-export GOMAXPROCS=8
+export GOMAXPROCS=10
 
 BINDIR=~/work/GitHub/evodevo3/bin
 genenv=${BINDIR}/genenv
@@ -11,11 +11,11 @@ POPSIZE=500
 ENVSFILE=Environments.json
 ${genenv} -o ${ENVSFILE} -n 50 -denv 0.5 -seed 13 2> environ.log
 
-EI1=30
+EI1=10
 EI0=`printf "%2.2d" $[${EI1}-1]`
-EI2=50
+EI2=12
 EEND=`printf "%2.2d" $[${EI2}-1]`
-MODELS=(Full)
+MODELS=(Full NoCue)
 #MODELS=(Full NoCue NoDev)
 #MODELS=(Hie0 Hie1 Hie2)
 #MODELS=(NullCue NullDev NullHie)
@@ -25,3 +25,17 @@ for model in $MODELS; do
 	> data/${model}_train.out
 done 2> train.log
 
+for model in $MODELS; do
+    ${runsim} -envs ${ENVSFILE} -production -env_start ${EI1} -env_end ${EI2} \
+	      -ngen 200 -setting traj/Setting_${model}.json \
+	      -restart traj/${model}_${EI0}_200.traj.gz \
+	       > data/${model}_test.out
+done 2> test.log
+
+for model in $MODELS; do
+    for epoch in {${EI1}..${EEND}}; do
+	echo epoch $epoch
+	${gpplot} -setting traj/Setting_${model}.json \
+ 		  -envs ${ENVSFILE} -ienv ${epoch} traj/${model}_${epoch}_*.traj.gz
+    done
+done 2> plot.log
