456273246825f280dc49d7c11dd947343ab94101
Akira R Kinjo <993982+arkinjo@users.noreply.github.com>
2025-01-13 15:32:54 +0900

    Update train.sh

--- scripts/train.sh
+++ scripts/train.sh
@@ -12,10 +12,11 @@ TRAIN=true
 TEST=true
 ANAL=true
 
-EI1=10 # epochs for training: [0, EI1)
-EI2=12 # epochs for testing (production run) [EI1, EI2)
+EI1=20 # epochs for training: [0, EI1)
+EI2=40 # epochs for testing (production run) [EI1, EI2)
 
 POPSIZE=500
+NGEN=200
 ENVSFILE=Environments.json
 if $ENVS; then
     ${genenv} -o ${ENVSFILE} -n 50 -denv 0.5 -seed 13 2> environ.log
@@ -24,14 +25,14 @@ fi
 EI0=`printf "%2.2d" $[${EI1}-1]`
 EEND=`printf "%2.2d" $[${EI2}-1]`
 
-MODELS=(Full NoCue NoDev)
-#MODELS=(Hie0 Hie1 Hie2)
+MODELS=(Full NoCue NoDev NoHie)
+#MODELS=(Hie1 Hie2)
 #MODELS=(NullCue NullDev NullHie)
 
 if $TRAIN; then
     for model in $MODELS; do
 	${runsim} -envs ${ENVSFILE} -model ${model} -popsize ${POPSIZE} \
-		  -env_start 0 -env_end ${EI1} -ngen 200 \
+		  -env_start 0 -env_end ${EI1} -ngen ${NGEN} \
 		  > data/${model}_train.out
     done 2> train.log
 fi
@@ -40,8 +41,8 @@ if $TEST; then
     for model in $MODELS; do
 	${runsim} -envs ${ENVSFILE} -production \
 		  -env_start ${EI1} -env_end ${EI2} \
-		  -ngen 200 -setting traj/Setting_${model}.json \
-		  -restart traj/${model}_${EI0}_200.traj.gz \
+		  -ngen ${NGEN} -setting traj/Setting_${model}.json \
+		  -restart traj/${model}_${EI0}_${NGEN}.traj.gz \
 		  > data/${model}_test.out
     done 2> test.log
 fi
@@ -49,10 +50,11 @@ fi
 if $ANAL; then
     for model in $MODELS; do
 	for epoch in {${EI1}..${EEND}}; do
-	    echo epoch $epoch
+	    echo epoch $epoch $ENVSFILE
 	    ${gpplot} -setting traj/Setting_${model}.json \
+		      -selected=true \
  		      -envs ${ENVSFILE} -ienv ${epoch} \
 		      traj/${model}_${epoch}_*.traj.gz
-	done
+	done 
     done 2> plot.log
 fi
